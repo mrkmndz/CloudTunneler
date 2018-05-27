df8ff42e73805ed20e25720afc76b3b8b4ceeab7
Mark Mendoza <mendoza.mark.a@gmail.com>
2018-05-27 01:37:30 +0000

    cleanup

--- scripts/create_instance.py
+++ scripts/create_instance.py
@@ -1,24 +1,3 @@
-# copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Example of using the Compute Engine API to create and delete instances.
-Creates a new compute engine instance and uses it to apply a caption to
-an image.
-    https://cloud.google.com/compute/docs/tutorials/python-guide
-For more information, see the README.md under /compute.
-"""
-
 import argparse
 import os
 import time
@@ -27,14 +6,6 @@ import googleapiclient.discovery
 from six.moves import input
 
 
-# [START list_instances]
-def list_instances(compute, project, zone):
-    result = compute.instances().list(project=project, zone=zone).execute()
-    return result['items']
-# [END list_instances]
-
-
-# [START create_instance]
 def create_instance(compute, project, zone, name, imageID, vcpus, script, script_params):
     # https://www.googleapis.com/compute/v1/projects/{project}/global/images/{resourceId}
     image_response = compute.images().get(project=project, image=imageID).execute()
@@ -96,19 +67,15 @@ def create_instance(compute, project, zone, name, imageID, vcpus, script, script
         project=project,
         zone=zone,
         body=config).execute()
-# [END create_instance]
 
 
-# [START delete_instance]
 def delete_instance(compute, project, zone, name):
     return compute.instances().delete(
         project=project,
         zone=zone,
         instance=name).execute()
-# [END delete_instance]
 
 
-# [START wait_for_operation]
 def wait_for_operation(compute, project, zone, operation):
     print('Waiting for operation to finish...')
     while True:
@@ -124,10 +91,8 @@ def wait_for_operation(compute, project, zone, operation):
             return result
 
         time.sleep(1)
-# [END wait_for_operation]
 
 
-# [START run]
 def main(project, zone, instance_name, imageID, vcpus,  wait=True):
     compute = googleapiclient.discovery.build('compute', 'v1')
 
