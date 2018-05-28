4728ce25f936fa397fa80abf7d6ebd690684056b
Mark Mendoza <mendoza.mark.a@gmail.com>
2018-05-28 00:18:55 +0000

    init pools in pairs

--- scripts/create_topo.py
+++ scripts/create_topo.py
@@ -29,17 +29,24 @@ def create_link(regionA, regionB, pools, clients):
                 external_private_keyB="tmp")
     return link
 
-def init_pool(src_region, dst_region):
+def init_pools(gcp, src_region, dst_region):
     # create pool
-    pool_name = gcp.create_pool(src_region, dst_region)
+    pool_nameA = gcp.create_pool(src_region, dst_region)
     # create forwarding rule
-    gcp.forward_to_pool(pool_name, src_region)
+    gcp.forward_to_pool(pool_nameA, src_region)
     # create pool
-    pool_name = gcp.create_pool(dst_region, src_region)
+    pool_nameB = gcp.create_pool(dst_region, src_region)
     # create forwarding rule
-    gcp.forward_to_pool(pool_name, dst_region)
+    gcp.forward_to_pool(pool_nameB, dst_region)
 
-    return pool_name
+    return pool_nameA, pool_nameB
+
+def get_or_init_pools(gcp, src_region, dst_region, pools):
+    if src_region in pools:
+        assert dst_region in pools
+        return pools[src_region], pools[dst_region]
+    else:
+        return init_pools(gcp, src_region, dst_region)
 
 def main(config_file):
     gcp = GCPController("proj-204902")
@@ -49,20 +56,19 @@ def main(config_file):
     # TODO more efficient way to wait for operations
     clients = {} # dict of all client objects... region -> client
     pools = {} # dict of created pools... region -> pool
+
     # create all clients
     for region in config["regions"]:
         # TODO only doing 1 client per region
         clients[region["name"]] = create_client()
-    # expand links
+
     for regionA in config["regions"]:
         for regionB in config["regions"]:
             if regionA["name"] != regionB["name"]:
-                pools[regionA["name"]] = pools[regionA["name"]] if regionA["name"] in pools else init_pool(regionA["name"], regionB["name"])
-                pools[regionB["name"]] = pools[regionB["name"]] if regionB["name"] in pools else init_pool(regionB["name"], regionA["name"])
+                pools[regionA["name"]], pools[regionB["name"]] = get_or_init_pools(gcp, regionA["name"], regionB["name"])
                 link = create_link(regionA, regionB, pools, clients)
                 pprint(link)
                 # expand_link(link)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
