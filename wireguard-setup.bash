c24aae423fb75f2170d2ffb311910d9e4fd80d2a
Chris Collins <chris.collins.1210@gmail.com>
2018-06-09 13:32:13 -0700

    wireguard install script

---
+++ wireguard-setup.bash
@@ -0,0 +1,5 @@
+sudo curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+
+sudo yum install epel-release
+
+sudo yum install wireguard-dkms wireguard-tools
